80b0c999ae00b8bd1a22e05ae1ba25868877e993
smallproject <j.a.villas@live.com.ph>
2024-08-09 11:30:01 +0200

    boiler plate code

---
+++ src/main/java/nl/smallproject/www/librarysystembackend/LibrarySystemBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.smallproject.www.librarysystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySystemBackendApplication.class, args);
+    }
+
+}
