90a6ab22374326a5cee543ddae540c445029af84
smallproject <j.a.villas@live.com.ph>
2024-09-16 15:07:43 +0200

    add a global exception handler - handles all out of scope exceptions and throws a not found status

---
+++ src/main/java/nl/smallproject/www/librarysystembackend/controllers/ExceptionController.java
@@ -0,0 +1,16 @@
+package nl.smallproject.www.librarysystembackend.controllers;
+
+import nl.smallproject.www.librarysystembackend.exceptions.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<Object> exception(RecordNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
