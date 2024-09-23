966a34df56dba5d63e797b8d15ad518d69ebc72a
smallproject <j.a.villas@live.com.ph>
2024-09-23 16:03:08 +0200

    -temporarily removed might not be needed

--- src/main/java/nl/smallproject/www/librarysystembackend/repositories/UserReviewRepository.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/repositories/UserReviewRepository.java
@@ -4,6 +4,6 @@ import nl.smallproject.www.librarysystembackend.models.UserReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
-@RepositoryRestController
+//@RepositoryRestController
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
 }
