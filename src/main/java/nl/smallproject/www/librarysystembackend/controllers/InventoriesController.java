60c9faee6697e7e947145909a7f8a7ad5bea2d21
smallproject <j.a.villas@live.com.ph>
2024-09-24 20:09:36 +0200

    Transformed transaction from controller level to service level transaction contains: -get -getAll -create -update -delete

--- src/main/java/nl/smallproject/www/librarysystembackend/controllers/InventoriesController.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/controllers/InventoriesController.java
@@ -2,35 +2,51 @@ package nl.smallproject.www.librarysystembackend.controllers;
 
 import nl.smallproject.www.librarysystembackend.models.Inventory;
 import nl.smallproject.www.librarysystembackend.repositories.InventoryRepository;
+import nl.smallproject.www.librarysystembackend.services.InventoryService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/v1/inventory")
 public class InventoriesController {
-    @Autowired
-    private InventoryRepository inventoryRepository;
+    private final InventoryService inventoryService;
 
-    @GetMapping
-    @RequestMapping("{id}")
-    public Inventory get(@PathVariable Long id) {
-        return inventoryRepository.getReferenceById(id);
+    public InventoriesController( InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
     }
 
-    @PostMapping("/addInventory")
-    public Inventory create(@RequestBody final Inventory inventory) {
-        return inventoryRepository.saveAndFlush(inventory);
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Inventory>> getAllInventories() {
+        List<Inventory> inventories = inventoryService.getAllInventories();
+        return ResponseEntity.ok(inventories);
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable Long id) {
-        inventoryRepository.deleteById(id);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Inventory> getInventoryById(@PathVariable Long id) {
+        Inventory existingInventory = inventoryService.getInventory(id);
+        return ResponseEntity.ok(existingInventory);
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    public Inventory update(@PathVariable Long id, @RequestBody final Inventory inventory) {
-        Inventory existingInventory = inventoryRepository.getReferenceById(id);
-        BeanUtils.copyProperties(inventory, existingInventory, "id");
-        return inventoryRepository.saveAndFlush(existingInventory);
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Inventory> createInventory(@RequestBody final Inventory inventory) {
+        Inventory savedInventory = inventoryService.saveInventory(inventory);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedInventory);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Inventory> update(@PathVariable Long id, @RequestBody final Inventory inventory) {
+        inventoryService.updateInventory(id, inventory);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Inventory> deleteInventory(@PathVariable Long id) {
+        inventoryService.deleteInventory(id);
+        return ResponseEntity.noContent().build();
     }
 }
