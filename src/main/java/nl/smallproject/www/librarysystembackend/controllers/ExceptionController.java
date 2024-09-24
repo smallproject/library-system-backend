98db535c30708ab09ee3888b27700e1b85e681bb
smallproject <j.a.villas@live.com.ph>
2024-09-24 13:05:49 +0200

    Added exception handling -catches exception on global level

--- src/main/java/nl/smallproject/www/librarysystembackend/controllers/ExceptionController.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/controllers/ExceptionController.java
@@ -1,10 +1,16 @@
 package nl.smallproject.www.librarysystembackend.controllers;
 
 import nl.smallproject.www.librarysystembackend.exceptions.RecordNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -12,5 +18,12 @@ public class ExceptionController {
     @ExceptionHandler(value = RecordNotFoundException.class)
     public ResponseEntity<Object> exception(RecordNotFoundException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
+
+        return new ResponseEntity<>(errors, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
