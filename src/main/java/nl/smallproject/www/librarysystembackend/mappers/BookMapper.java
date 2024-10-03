455ee43dbc56ff8b6538b762042a4be9bcf937f8
smallproject <j.a.villas@live.com.ph>
2024-10-03 22:31:44 +0200

    updated mapper -adding in new fields -title -genre -pagecount -language -coverimageurl -descriptionsummary -rating -copiesavailable -dateadded -status

--- src/main/java/nl/smallproject/www/librarysystembackend/mappers/BookMapper.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/mappers/BookMapper.java
@@ -12,27 +12,51 @@ public class BookMapper {
         BookOutputDto bookOutputDto = new BookOutputDto();
         bookOutputDto.setId(book.getId());
         bookOutputDto.setIsbn(book.getIsbn());
-        bookOutputDto.setName(book.getName());
-        bookOutputDto.setDescription(book.getDescription());
+        bookOutputDto.setTitle(book.getTitle());
         bookOutputDto.setPublicationDate(book.getPublicationDate());
+        bookOutputDto.setGenre(book.getGenre());
+        bookOutputDto.setPagecount(book.getPageCount());
+        bookOutputDto.setLanguage(book.getLanguage());
+        bookOutputDto.setCoverImageUrl(book.getCoverImageUrl());
+        bookOutputDto.setDescriptionsummary(book.getDescriptionSummary());
+        bookOutputDto.setRating(book.getRating());
+        bookOutputDto.setCopiesAvailable(book.getCopiesAvailable());
+        bookOutputDto.setDateAdded(book.getDateAdded());
+        bookOutputDto.setStatus(book.getStatus());
         return bookOutputDto;
     }
 
     public Book bookInputDtoToEntity(BookInputDto bookInputDto) {
         Book book = new Book();
         book.setIsbn(bookInputDto.getIsbn());
-        book.setName(bookInputDto.getName());
-        book.setDescription(bookInputDto.getDescription());
+        book.setTitle(bookInputDto.getTitle());
         book.setPublicationDate(bookInputDto.getPublicationDate());
+        book.setGenre(bookInputDto.getGenre());
+        book.setPageCount(bookInputDto.getPageCount());
+        book.setLanguage(bookInputDto.getLanguage());
+        book.setCoverImageUrl(bookInputDto.getCoverImageUrl());
+        book.setDescriptionSummary(bookInputDto.getDescriptionSummary());
+        book.setRating(bookInputDto.getRating());
+        book.setCopiesAvailable(bookInputDto.getCopiesAvailable());
+        book.setDateAdded(bookInputDto.getDateAdded());
+        book.setStatus(bookInputDto.getStatus());
         return book;
     }
 
     public Book bookUpdateDtoToEntity(BookUpdateDto bookUpdateDto) {
         Book book = new Book();
         book.setIsbn(bookUpdateDto.getIsbn());
-        book.setName(bookUpdateDto.getName());
-        book.setDescription(bookUpdateDto.getDescription());
+        book.setTitle(bookUpdateDto.getTitle());
         book.setPublicationDate(bookUpdateDto.getPublicationDate());
+        book.setGenre(bookUpdateDto.getGenre());
+        book.setPageCount(bookUpdateDto.getPageCount());
+        book.setLanguage(bookUpdateDto.getLanguage());
+        book.setCoverImageUrl(bookUpdateDto.getCoverImageUrl());
+        book.setDescriptionSummary(bookUpdateDto.getDescriptionSummary());
+        book.setRating(bookUpdateDto.getRating());
+        book.setCopiesAvailable(bookUpdateDto.getCopiesAvailable());
+        book.setDateAdded(bookUpdateDto.getDateAdded());
+        book.setStatus(bookUpdateDto.getStatus());
         return book;
     }
 }
