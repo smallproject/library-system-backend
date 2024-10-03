f5c30e92a71096093a30f49abea666606423c40d
smallproject <j.a.villas@live.com.ph>
2024-10-03 19:20:20 +0200

    added getters and setters

--- src/main/java/nl/smallproject/www/librarysystembackend/models/Book.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/models/Book.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 import java.util.Date;
 
-@Data
+//@Data
 @Entity(name = "books")
 public class Book {
     @Id
@@ -23,4 +23,44 @@ public class Book {
 
     @Column(name = "publicationdate")
     private Date publicationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
 }
