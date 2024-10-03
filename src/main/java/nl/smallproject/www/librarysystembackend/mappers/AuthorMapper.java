08bc74a754529eb5daa5c95f34857d5252d190ae
smallproject <j.a.villas@live.com.ph>
2024-10-03 17:18:59 +0200

    added author mapper - dto to entity

--- src/main/java/nl/smallproject/www/librarysystembackend/mappers/AuthorMapper.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/mappers/AuthorMapper.java
@@ -1,5 +1,6 @@
 package nl.smallproject.www.librarysystembackend.mappers;
 
+import nl.smallproject.www.librarysystembackend.dtos.AuthorInputDto;
 import nl.smallproject.www.librarysystembackend.dtos.AuthorOutputDto;
 import nl.smallproject.www.librarysystembackend.models.Author;
 import org.springframework.stereotype.Component;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthorMapper {
 
-    public AuthorOutputDto AuthorEntityToOutputDto(Author author) {
+    public AuthorOutputDto authorEntityToOutputDto(Author author) {
         AuthorOutputDto authorOutputDto = new AuthorOutputDto();
         authorOutputDto.setId(author.getId());
         authorOutputDto.setFirstName(author.getFirstName());
@@ -23,5 +24,22 @@ public class AuthorMapper {
         authorOutputDto.setActiveYears(author.getActiveYears());
         authorOutputDto.setProfilePictureUrl(author.getProfilePictureUrl());
         return authorOutputDto;
+    }
+
+    public Author authorInputDtoToEntity(AuthorInputDto authorInputDto) {
+        Author author = new Author();
+        author.setFirstName(authorInputDto.getFirstName());
+        author.setMiddleName(authorInputDto.getMiddleName());
+        author.setLastName(authorInputDto.getLastName());
+        author.setDateOfBirth(authorInputDto.getDateOfBirth());
+        author.setNationality(authorInputDto.getNationality());
+        author.setDateOfDeath(authorInputDto.getDateOfDeath());
+        author.setBio(authorInputDto.getBio());
+        author.setWebsite(authorInputDto.getWebsite());
+        author.setEmail(authorInputDto.getEmail());
+        author.setAwards(authorInputDto.getAwards());
+        author.setActiveYears(authorInputDto.getActiveYears());
+        author.setProfilePictureUrl(authorInputDto.getProfilePictureUrl());
+        return author;
     }
 }
