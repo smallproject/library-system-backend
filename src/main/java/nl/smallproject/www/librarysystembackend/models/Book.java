7a9cf408d2803f16492130138a43a8097ea18e2b
smallproject <j.a.villas@live.com.ph>
2024-10-03 21:51:14 +0200

    added more fields to book entity -genre -pagecount -language -coverimageurl -descriptionsummary -rating -copiesavailable -dateadded -status

--- src/main/java/nl/smallproject/www/librarysystembackend/models/Book.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/models/Book.java
@@ -1,9 +1,9 @@
 package nl.smallproject.www.librarysystembackend.models;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 //@Data
 @Entity(name = "books")
@@ -15,14 +15,42 @@ public class Book {
     @Column(name = "isbn")
     private String isbn;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "title")
+    private String title;
 
-    @Column(name = "description")
-    private String description;
+//    this needs relation
+//    private List<Author> authors;
+
 
     @Column(name = "publicationdate")
     private Date publicationDate;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "pagecount")
+    private Integer pagecount;
+
+    @Column(name = "language")
+    private String language;
+
+    @Column(name = "coverimageurl")
+    private String coverImageUrl;
+
+    @Column(name = "descriptionsummary")
+    private String descriptionsummary;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    @Column(name = "copiesavailable")
+    private Integer copiesAvailable;
+
+    @Column(name = "dateadded")
+    private Date dateAdded;
+
+    @Column(name = "status")
+    private String status;
 
     public Long getId() {
         return id;
@@ -40,20 +68,12 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public Date getPublicationDate() {
@@ -62,5 +82,77 @@ public class Book {
 
     public void setPublicationDate(Date publicationDate) {
         this.publicationDate = publicationDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getPagecount() {
+        return pagecount;
+    }
+
+    public void setPagecount(Integer pagecount) {
+        this.pagecount = pagecount;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCoverImageUrl() {
+        return coverImageUrl;
+    }
+
+    public void setCoverImageUrl(String coverImageUrl) {
+        this.coverImageUrl = coverImageUrl;
+    }
+
+    public String getDescriptionsummary() {
+        return descriptionsummary;
+    }
+
+    public void setDescriptionsummary(String descriptionsummary) {
+        this.descriptionsummary = descriptionsummary;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Integer getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public void setCopiesAvailable(Integer copiesAvailable) {
+        this.copiesAvailable = copiesAvailable;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
