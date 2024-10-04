c4f2453b3cefc652c4e6d9cbc4ce9c50be264328
smallproject <j.a.villas@live.com.ph>
2024-10-04 13:46:31 +0200

    added new fields for user review -bookid -rating -reviewtext -reviewtitle -reviewdate -helpfulcount -flagged -flaggedreason -responsecount -updateat -userrecommendation -readstatus -readdate -userlocation -spoilerflag -responsefromlibrarian -anonymousreview

--- src/main/java/nl/smallproject/www/librarysystembackend/models/UserReview.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/models/UserReview.java
@@ -2,6 +2,8 @@ package nl.smallproject.www.librarysystembackend.models;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity(name = "userreviews")
 public class UserReview {
     @Id
@@ -11,11 +13,56 @@ public class UserReview {
     @Column(name = "userid")
     private String userId;
 
-    @Column(name = "rating")
-    private int rating;
+    @Column(name = "bookid")
+    private Long bookId;
 
-    @Column(name = "comments")
-    private String comments;
+    @Column(name = "rating")
+    private Integer rating;
+
+    @Column(name = "reviewtext")
+    private String reviewText;
+
+    @Column(name = "reviewtitle")
+    private String reviewTitle;
+
+    @Column(name = "reviewdate")
+    private Date reviewDate;
+
+    @Column(name = "helpfulcount")
+    private Integer helpfulCount;
+
+    @Column(name = "flagged")
+    private Boolean flagged;
+
+    @Column(name = "flaggedreason")
+    private String flaggedReason;
+
+    @Column(name = "responsecount")
+    private Integer responseCount;
+
+    @Column(name = "updateat")
+    private Date updatedAt;
+
+    @Column(name = "userrecommendation")
+    private Boolean userRecommendation;
+
+    @Column(name = "readstatus")
+    private String readStatus;
+
+    @Column(name = "readdate")
+    private Date readDate;
+
+    @Column(name = "userlocation")
+    private String userLocation;
+
+    @Column(name = "spoilerflag")
+    private Boolean spoilerFlag;
+
+    @Column(name = "responsefromlibrarian")
+    private String responseFromLibrarian;
+
+    @Column(name = "anonymousreview")
+    private Boolean anonymousReview;
 
     public Long getId() {
         return id;
@@ -33,19 +80,139 @@ public class UserReview {
         this.userId = userId;
     }
 
-    public int getRating() {
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Integer getRating() {
         return rating;
     }
 
-    public void setRating(int rating) {
+    public void setRating(Integer rating) {
         this.rating = rating;
     }
 
-    public String getComments() {
-        return comments;
+    public String getReviewText() {
+        return reviewText;
     }
 
-    public void setComments(String comments) {
-        this.comments = comments;
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public String getReviewTitle() {
+        return reviewTitle;
+    }
+
+    public void setReviewTitle(String reviewTitle) {
+        this.reviewTitle = reviewTitle;
+    }
+
+    public Date getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(Date reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    public Integer getHelpfulCount() {
+        return helpfulCount;
+    }
+
+    public void setHelpfulCount(Integer helpfulCount) {
+        this.helpfulCount = helpfulCount;
+    }
+
+    public Boolean getFlagged() {
+        return flagged;
+    }
+
+    public void setFlagged(Boolean flagged) {
+        this.flagged = flagged;
+    }
+
+    public String getFlaggedReason() {
+        return flaggedReason;
+    }
+
+    public void setFlaggedReason(String flaggedReason) {
+        this.flaggedReason = flaggedReason;
+    }
+
+    public Integer getResponseCount() {
+        return responseCount;
+    }
+
+    public void setResponseCount(Integer responseCount) {
+        this.responseCount = responseCount;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Boolean getUserRecommendation() {
+        return userRecommendation;
+    }
+
+    public void setUserRecommendation(Boolean userRecommendation) {
+        this.userRecommendation = userRecommendation;
+    }
+
+    public String getReadStatus() {
+        return readStatus;
+    }
+
+    public void setReadStatus(String readStatus) {
+        this.readStatus = readStatus;
+    }
+
+    public Date getReadDate() {
+        return readDate;
+    }
+
+    public void setReadDate(Date readDate) {
+        this.readDate = readDate;
+    }
+
+    public String getUserLocation() {
+        return userLocation;
+    }
+
+    public void setUserLocation(String userLocation) {
+        this.userLocation = userLocation;
+    }
+
+    public Boolean getSpoilerFlag() {
+        return spoilerFlag;
+    }
+
+    public void setSpoilerFlag(Boolean spoilerFlag) {
+        this.spoilerFlag = spoilerFlag;
+    }
+
+    public String getResponseFromLibrarian() {
+        return responseFromLibrarian;
+    }
+
+    public void setResponseFromLibrarian(String responseFromLibrarian) {
+        this.responseFromLibrarian = responseFromLibrarian;
+    }
+
+    public Boolean getAnonymousReview() {
+        return anonymousReview;
+    }
+
+    public void setAnonymousReview(Boolean anonymousReview) {
+        this.anonymousReview = anonymousReview;
     }
 }
