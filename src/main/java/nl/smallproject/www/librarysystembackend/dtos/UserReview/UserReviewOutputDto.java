39b12b518e8a7d5573cb92036d8d42d569d80212
smallproject <j.a.villas@live.com.ph>
2024-10-04 13:48:17 +0200

    added new fields for user review output dto -bookid -rating -reviewtext -reviewtitle -reviewdate -helpfulcount -flagged -flaggedreason -responsecount -updateat -userrecommendation -readstatus -readdate -userlocation -spoilerflag -responsefromlibrarian -anonymousreview

--- src/main/java/nl/smallproject/www/librarysystembackend/dtos/UserReview/UserReviewOutputDto.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/dtos/UserReview/UserReviewOutputDto.java
@@ -1,13 +1,50 @@
 package nl.smallproject.www.librarysystembackend.dtos.UserReview;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
 public class UserReviewOutputDto {
     private Long id;
 
     private String userId;
 
-    private int rating;
+    private Long bookId;
 
-    private String comments;
+    private Integer rating;
+
+    private String reviewText;
+
+    private String reviewTitle;
+
+    private Date reviewDate;
+
+    private Integer helpfulCount;
+
+    private Boolean flagged;
+
+    private String flaggedReason;
+
+    private Integer responseCount;
+
+    private Date updatedAt;
+
+    private Boolean userRecommendation;
+
+    private String readStatus;
+
+    private Date readDate;
+
+    private String userLocation;
+
+    private Boolean spoilerFlag;
+
+    private String responseFromLibrarian;
+
+    private Boolean anonymousReview;
 
     public Long getId() {
         return id;
@@ -25,19 +62,139 @@ public class UserReviewOutputDto {
         this.userId = userId;
     }
 
-    public int getRating() {
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Integer getRating() {
         return rating;
     }
 
-    public void setRating(int rating) {
+    public void setRating(Integer rating) {
         this.rating = rating;
     }
 
-    public String getComments() {
-        return comments;
+    public String getReviewText() {
+        return reviewText;
     }
 
-    public void setComments(String comments) {
-        this.comments = comments;
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public String getReviewTitle() {
+        return reviewTitle;
+    }
+
+    public void setReviewTitle(String reviewTitle) {
+        this.reviewTitle = reviewTitle;
+    }
+
+    public Date getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(Date reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    public Integer getHelpfulCount() {
+        return helpfulCount;
+    }
+
+    public void setHelpfulCount(Integer helpfulCount) {
+        this.helpfulCount = helpfulCount;
+    }
+
+    public Boolean getFlagged() {
+        return flagged;
+    }
+
+    public void setFlagged(Boolean flagged) {
+        this.flagged = flagged;
+    }
+
+    public String getFlaggedReason() {
+        return flaggedReason;
+    }
+
+    public void setFlaggedReason(String flaggedReason) {
+        this.flaggedReason = flaggedReason;
+    }
+
+    public Integer getResponseCount() {
+        return responseCount;
+    }
+
+    public void setResponseCount(Integer responseCount) {
+        this.responseCount = responseCount;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Boolean getUserRecommendation() {
+        return userRecommendation;
+    }
+
+    public void setUserRecommendation(Boolean userRecommendation) {
+        this.userRecommendation = userRecommendation;
+    }
+
+    public String getReadStatus() {
+        return readStatus;
+    }
+
+    public void setReadStatus(String readStatus) {
+        this.readStatus = readStatus;
+    }
+
+    public Date getReadDate() {
+        return readDate;
+    }
+
+    public void setReadDate(Date readDate) {
+        this.readDate = readDate;
+    }
+
+    public String getUserLocation() {
+        return userLocation;
+    }
+
+    public void setUserLocation(String userLocation) {
+        this.userLocation = userLocation;
+    }
+
+    public Boolean getSpoilerFlag() {
+        return spoilerFlag;
+    }
+
+    public void setSpoilerFlag(Boolean spoilerFlag) {
+        this.spoilerFlag = spoilerFlag;
+    }
+
+    public String getResponseFromLibrarian() {
+        return responseFromLibrarian;
+    }
+
+    public void setResponseFromLibrarian(String responseFromLibrarian) {
+        this.responseFromLibrarian = responseFromLibrarian;
+    }
+
+    public Boolean getAnonymousReview() {
+        return anonymousReview;
+    }
+
+    public void setAnonymousReview(Boolean anonymousReview) {
+        this.anonymousReview = anonymousReview;
     }
 }
