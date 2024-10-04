72748a48e2538a17859da8d7c1e5320d44df1f56
smallproject <j.a.villas@live.com.ph>
2024-10-04 09:12:40 +0200

    added an update dto mapper for user review -added validation -added appropriate validation messages -added getters and setters

---
+++ src/main/java/nl/smallproject/www/librarysystembackend/dtos/UserReviewUpdateDto.java
@@ -0,0 +1,44 @@
+package nl.smallproject.www.librarysystembackend.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+public class UserReviewUpdateDto {
+
+    @NotBlank(message = "{userReview.userId.notblank}")
+    @Size(min = 3, max = 255, message = "{userReview.userId.size}")
+    private String userId;
+
+    @PositiveOrZero(message = "{userReview.rating.positiveornotzero}")
+    private int rating;
+
+    @NotBlank(message = "{userReview.comments.notblank}")
+    @Size(min = 3, max = 255, message = "{userReview.comments.size}")
+    private String comments;
+
+    public @NotBlank(message = "{userReview.userId.notblank}") @Size(min = 3, max = 255, message = "{userReview.userId.size}") String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(@NotBlank(message = "{userReview.userId.notblank}") @Size(min = 3, max = 255, message = "{userReview.userId.size}") String userId) {
+        this.userId = userId;
+    }
+
+    @PositiveOrZero(message = "{userReview.rating.positiveornotzero}")
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(@PositiveOrZero(message = "{userReview.rating.positiveornotzero}") int rating) {
+        this.rating = rating;
+    }
+
+    public @NotBlank(message = "{userReview.comments.notblank}") @Size(min = 3, max = 255, message = "{userReview.comments.size}") String getComments() {
+        return comments;
+    }
+
+    public void setComments(@NotBlank(message = "{userReview.comments.notblank}") @Size(min = 3, max = 255, message = "{userReview.comments.size}") String comments) {
+        this.comments = comments;
+    }
+}
