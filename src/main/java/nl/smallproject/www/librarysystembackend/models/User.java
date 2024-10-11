6039ede26475a7a75fefd968882c90f3978fbc20
smallproject <j.a.villas@live.com.ph>
2024-10-11 15:20:09 +0200

    added user table for authorization set up proper column names added getters and setters

---
+++ src/main/java/nl/smallproject/www/librarysystembackend/models/User.java
@@ -0,0 +1,75 @@
+package nl.smallproject.www.librarysystembackend.models;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "passwordhash")
+    private String passwordHash;
+
+    @Column(name = "createdat")
+    private Date createdAt;
+
+    @Column(name = "updatedat")
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String password_hash) {
+        this.passwordHash = password_hash;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
