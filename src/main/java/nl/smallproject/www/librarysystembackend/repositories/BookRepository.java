18dc919946cad1526d99337bfac5b17bcd794ea6
smallproject <j.a.villas@live.com.ph>
2024-10-18 09:59:53 +0200

    added repository annotation

--- src/main/java/nl/smallproject/www/librarysystembackend/repositories/BookRepository.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/repositories/BookRepository.java
@@ -3,7 +3,8 @@ package nl.smallproject.www.librarysystembackend.repositories;
 import nl.smallproject.www.librarysystembackend.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.stereotype.Repository;
 
-//@RepositoryRestController
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
