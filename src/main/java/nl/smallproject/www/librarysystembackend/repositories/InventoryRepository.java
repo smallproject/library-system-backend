de984043d48322ae5ddce74d7b65d4b81230d581
smallproject <j.a.villas@live.com.ph>
2024-10-18 10:00:02 +0200

    added repository annotation

--- src/main/java/nl/smallproject/www/librarysystembackend/repositories/InventoryRepository.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/repositories/InventoryRepository.java
@@ -3,7 +3,9 @@ package nl.smallproject.www.librarysystembackend.repositories;
 import nl.smallproject.www.librarysystembackend.models.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.stereotype.Repository;
 
+@Repository
 //@RepositoryRestController
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 }
