8536afb68e350a46064f238d63c9c45631874ec1
smallproject <j.a.villas@live.com.ph>
2024-10-23 09:39:46 +0200

    added user repository

--- src/main/java/nl/smallproject/www/librarysystembackend/repositories/UserRepository.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package nl.smallproject.www.librarysystembackend.repositories;
 
-import nl.smallproject.www.librarysystembackend.models.User;
+import nl.smallproject.www.librarysystembackend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUserName(String username);
+
+    Optional<User> findByUserNameAndPassword(String username, String password);
 }
