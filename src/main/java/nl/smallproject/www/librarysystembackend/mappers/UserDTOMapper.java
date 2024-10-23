5b8158b756b83e8cbdcb2a692afab13c00864302
smallproject <j.a.villas@live.com.ph>
2024-10-23 09:43:55 +0200

    added user dto mapper

---
+++ src/main/java/nl/smallproject/www/librarysystembackend/mappers/UserDTOMapper.java
@@ -0,0 +1,26 @@
+package nl.smallproject.www.librarysystembackend.mappers;
+
+
+import nl.smallproject.www.librarysystembackend.dtos.Authentication.UserChangePassWordRequestDTO;
+import nl.smallproject.www.librarysystembackend.dtos.Authentication.UserRequestDTO;
+import nl.smallproject.www.librarysystembackend.models.UserModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDTOMapper {
+
+
+
+    public UserModel mapToModel(UserRequestDTO userDTO) {
+        var result = new UserModel(-1L);
+        result.setUserName(userDTO.getUserName());
+        result.setPassword(userDTO.getPassword());
+        return result;
+    }
+
+    public UserModel mapToModel(UserChangePassWordRequestDTO userDTO, Long id) {
+        var result = new UserModel(id);
+        result.setPassword(userDTO.getPassword());
+        return result;
+    }
+}
