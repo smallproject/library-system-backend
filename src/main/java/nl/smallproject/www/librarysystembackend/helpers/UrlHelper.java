4cf2727c2d9dfa7da20f5dcf717cda4e6eac3288
smallproject <j.a.villas@live.com.ph>
2024-10-23 10:40:09 +0200

    added a urlHelper for to String conversion

---
+++ src/main/java/nl/smallproject/www/librarysystembackend/helpers/UrlHelper.java
@@ -0,0 +1,28 @@
+package nl.smallproject.www.librarysystembackend.helpers;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.net.URI;
+
+public class UrlHelper {
+    public static String getCurrentUrlString(HttpServletRequest request) {
+        return request.getRequestURL().toString();
+    }
+
+    public static String getCurrentUrlString(HttpServletRequest request, Long id) {
+        return request.getRequestURL().toString() + "/" + id.toString();
+    }
+
+    public static URI getCurrentUrl(HttpServletRequest request) {
+        return convertToURI(getCurrentUrlString(request));
+    }
+
+    public static URI getCurrentUrlWithId(HttpServletRequest request, Long id) {
+        return convertToURI(getCurrentUrlString(request, id));
+    }
+
+    private static URI convertToURI(String uri) {
+        return URI.create(uri);
+    }
+}
