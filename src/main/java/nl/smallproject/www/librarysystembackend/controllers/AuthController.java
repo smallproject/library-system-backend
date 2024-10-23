86cccc6032dd9e8962a4f3b0e88a3c3bcc3c86f4
smallproject <j.a.villas@live.com.ph>
2024-10-23 10:40:51 +0200

    added login endpoint for token creation, jwt implementation

---
+++ src/main/java/nl/smallproject/www/librarysystembackend/controllers/AuthController.java
@@ -0,0 +1,46 @@
+package nl.smallproject.www.librarysystembackend.controllers;
+
+import nl.smallproject.www.librarysystembackend.dtos.Authentication.UserLoginRequestDTO;
+import nl.smallproject.www.librarysystembackend.security.ApiUserDetails;
+import nl.smallproject.www.librarysystembackend.security.JwtService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    private final AuthenticationManager authManager;
+    private final JwtService jwtService;
+
+    public AuthController(AuthenticationManager man, JwtService service) {
+        this.authManager = man;
+        this.jwtService = service;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody UserLoginRequestDTO userLoginRequestDTO
+    ) {
+        UsernamePasswordAuthenticationToken up =
+                new UsernamePasswordAuthenticationToken(userLoginRequestDTO.getUserName(), userLoginRequestDTO.getPassword());
+
+        try {
+            Authentication auth = authManager.authenticate(up);
+
+            var ud = (ApiUserDetails) auth.getPrincipal();
+            String token = jwtService.generateToken(ud);
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                    .body("Token generated");
+        } catch (AuthenticationException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
