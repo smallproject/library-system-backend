3cb27f84e62af17a4badf372b78f023f799d06ff
smallproject <j.a.villas@live.com.ph>
2024-10-23 09:44:55 +0200

    added user change password request dto

---
+++ src/main/java/nl/smallproject/www/librarysystembackend/dtos/Authentication/UserChangePassWordRequestDTO.java
@@ -0,0 +1,13 @@
+package nl.smallproject.www.librarysystembackend.dtos.Authentication;
+
+public class UserChangePassWordRequestDTO {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
