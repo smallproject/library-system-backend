c297d76a4a1dbb04b51526e331c59475f38b7736
smallproject <j.a.villas@live.com.ph>
2024-10-23 12:58:23 +0200

    refactored so that is used the helper function

--- src/main/java/nl/smallproject/www/librarysystembackend/controllers/SecureController.java
+++ src/main/java/nl/smallproject/www/librarysystembackend/controllers/SecureController.java
@@ -1,50 +1,40 @@
 package nl.smallproject.www.librarysystembackend.controllers;
 
+import nl.smallproject.www.librarysystembackend.helpers.AuthenticationHelper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.stream.Collectors;
 
 @RestController
 public class SecureController {
-
     private Authentication authentication;
 
-    @GetMapping("/secure")
-    public ResponseEntity<String> getSecureData() {
-        setAuthentication(SecurityContextHolder.getContext());
-        return ResponseEntity.ok("Dit is beveiligde data: " + getAuthenticationText(authentication));
-    }
+    private final AuthenticationHelper authenticationHelper = new AuthenticationHelper();
 
     private void setAuthentication(SecurityContext context) {
         this.authentication =context.getAuthentication();
     }
 
+    @GetMapping("/secure")
+    public ResponseEntity<String> getSecureData() {
+        setAuthentication(SecurityContextHolder.getContext());
+        return ResponseEntity.ok("Dit is beveiligde data: " + authenticationHelper.getAuthenticationText(authentication));
+    }
+
     @GetMapping("/secure/admin")
     public ResponseEntity<String> getAdminData() {
         setAuthentication(SecurityContextHolder.getContext());
-        return ResponseEntity.ok("Dit is beveiligde admin data: " + getAuthenticationText(authentication));
+        return ResponseEntity.ok("Dit is beveiligde admin data: " + authenticationHelper.getAuthenticationText(authentication));
     }
 
     @GetMapping("/secure/user")
     public ResponseEntity<String> getUserData() {
         setAuthentication(SecurityContextHolder.getContext());
-        return ResponseEntity.ok("Dit is beveiligde user data: " +  getAuthenticationText(authentication));
+        return ResponseEntity.ok("Dit is beveiligde user data: " +  authenticationHelper.getAuthenticationText(authentication));
     }
 
-    private String getAuthenticationText(Authentication authentication){
-
-        return authentication.getPrincipal().toString() + " - Authorities: " + getAuthoritiesAsString(authentication) ;
-    }
-
-    public String getAuthoritiesAsString(Authentication authentication) {
-        return authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
-    }
 }
